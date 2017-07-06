ba5c2767b63f3596218b266887955dd82a4376e4
William Hill <hill119@llnl.gov>
2017-07-06 12:01:04 -0700

    working code

--- turtle_man.py
+++ turtle_man.py
@@ -1,30 +1,90 @@
 import turtle
 import random
+import math
 
+#Turtle objec to draw circles
 drawer = turtle.Turtle()
 drawer.hideturtle()
 
+#franklin!!!!
 franklin = turtle.Turtle()
+
+#A List of colors
+COLORS = ['red', 'blue', 'green', 'yellow', 'black', 'pink', 'gold', 'violet', 'orange', 'magenta', 'cyan']
+
+#A list that will hold the circle center point coordinates
+point_list = []
+
+#Sets the radius for all circles
+CIRCLE_RADIUS = 8
+
+#tuples to hold the range of x and y
+range_x = (0.0,300.0)
+range_y = (0.0, 300.0)
 
 #Set turtle's initial shape to a turtle
 franklin.shape("turtle")
 
+#Variable for holding franklin's calculated coordinate
+franklin_coordinate = None
+
 #Get the screen object
 screen = turtle.Screen()
 
-point_list = []
-DOT_RADIUS = 3
-range_x = (0,300)
-range_y = (0, 300)
+#Move the turtle object forward by 1 pixel
+def move_right():
+    global franklin_coordinate
+    global point_list
+    franklin.forward(1)
 
+    franklin_x = int(franklin.pos()[0])
+    franklin_y = int(franklin.pos()[1])
+    franklin_coordinate = (franklin_x, franklin_y)
+    print "franklin_coordinate: ", franklin_coordinate
+    is_in_circle(franklin_coordinate, point_list)
+
+#Move the turtle object backward by 1 pixel
+def move_left():
+    global franklin_coordinate
+    global point_list
+    franklin.forward(-1)
+
+    franklin_x = int(franklin.pos()[0])
+    franklin_y = int(franklin.pos()[1])
+
+    franklin_coordinate = (franklin_x, franklin_y)
+    print "franklin_coordinate: ", franklin_coordinate
+
+    is_in_circle(franklin_coordinate, point_list)
+
+#Turn the turtle counterclockwise by 1 degree
+def turn_left():
+    franklin.left(3)
+
+#Turn the turtle clockwise by 1 degree
+def turn_right():
+    franklin.right(3)
+
+
+#Move turtle to coordinate
 def move_turtle_to(position):
     drawer.up()
     drawer.goto(position[0], position[1])
     drawer.down()
 
-def draw_dot(radius=DOT_RADIUS):
-    drawer.dot(DOT_RADIUS)
+#function to draw circle
+def draw_circle(radius=CIRCLE_RADIUS):
+    drawer.circle(CIRCLE_RADIUS)
 
+#Check to see if Franklin is inside a circle
+def is_in_circle(position, point_list):
+    for circle in point_list:
+        location = math.sqrt((position[0] - circle[0]) ** 2 + (position[1] - circle[1]) ** 2)
+        if location < CIRCLE_RADIUS:
+            print "found circle"
+            franklin.fillcolor(random.choice(COLORS))
+
+#loop to populate point_list
 for _ in range(10):
     x_coor = random.randrange(*range_x)
     y_coor = random.randrange(*range_y)
@@ -32,9 +92,21 @@ for _ in range(10):
 
 print "point_list:", point_list
 
+#for loop for creating circles
 for point in point_list:
     move_turtle_to(point)
-    draw_dot(point)
+    draw_circle(point)
+
+print "franklin's coordinates: ", franklin.pos()
+
+#Set the screen.onkey functions
+screen.onkey(move_right, "Up")
+screen.onkey(move_left, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+
+#Add event listener to Screen
+screen.listen()
 
 #mainloop
 turtle.mainloop()
